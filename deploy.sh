f48cb4cba62ad9bd2120b7bf12429748f697a8b0
Matt McCants <matt@moduscreate.com>
2016-01-15 12:40:16 -0500

    Add oauth as env var.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERNAME=$(curl https://api.github.com/repos/MarriottLabs/mcom-performance/pulls/$ghprbPullId?access_token=06894fe9104bf3dfa8360a62146222bc664e8eda | jq -r .user.login)
+USERNAME=$(curl https://api.github.com/repos/MarriottLabs/mcom-performance/pulls/$ghprbPullId?access_token=$GH_OAUTH | jq -r .user.login)
 aws s3 mb s3://mcom-hackathon-${USERNAME}
 aws s3 sync --acl public-read --delete ratelist-menu/ s3://mcom-hackathon-${USERNAME}/
 aws s3 website s3://mcom-hackathon-${USERNAME}/ --index-document index.html
