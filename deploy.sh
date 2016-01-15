6afff0776575de57d4ea28b3bb6406e71e7b77ba
Matt McCants <matt@moduscreate.com>
2016-01-15 12:10:52 -0500

    Add an artificial sleep

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 USERNAME=$(curl https://api.github.com/repos/MarriottLabs/mcom-performance/pulls/$ghprbPullId?access_token=06894fe9104bf3dfa8360a62146222bc664e8eda | jq -r .user.login)
-
+sleep 2
 aws s3 mb s3://mcom-perf-${USERNAME}
+sleep 2
 aws s3 website s3://mcom-perf-${USERNAME}/ --index-document index.html
+sleep 2
 aws s3 sync --acl public-read --delete ratelist-menu/ s3://mcom-perf-${USERNAME}/
